6b9ad6d770aa07e86ea0d6f374151b20b761a682
shinoby85 <ponk@tut.by>
2020-09-06 11:16:49 +0300

    feat: add code to count-cats, carbon-dating and dream-team

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,14 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
+module.exports = function dateSample(sampleActivity) {
+  //throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  if (typeof sampleActivity!=="string" || Number(sampleActivity)>MODERN_ACTIVITY || Number(sampleActivity)<=0 || isNaN(Number(sampleActivity))){
+    return false;
+  }
+  let k=0.693/HALF_LIFE_PERIOD
+  let t=Math.log(MODERN_ACTIVITY/Number(sampleActivity))/k
+  return Math.ceil(t)
+
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,9 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+    //throw new CustomError('Not implemented');
+    // remove line with error and write your code here
+    if (!Array.isArray(members)) return false
+    return members.map(item=>typeof (item)==="string"?item.trim()[0].toUpperCase():'').sort().join('')
+
 };
