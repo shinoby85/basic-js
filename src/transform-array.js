840cc06ce06fb0b05ecf11d370a98bb1aad6082c
shinoby85 <ponk@tut.by>
2020-09-06 22:27:58 +0300

    feat: add code to transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,38 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+    //throw new CustomError('Not implemented');
+    if(!Array.isArray(arr)) throw Error('Param is not array')
+    let flagDiscard = false
+    return  arr.map((item, index, array) => {
+        if (flagDiscard) {
+            flagDiscard = false
+            return '';
+        }
+        if (index > 0) {
+            if (array[index] === '--double-prev') {
+                if(index>1 && array[index-2]==='--discard-next') return ''
+                return array[index - 1]
+            }
+        }
+        if (index < array.length - 1) {
+
+            if (array[index + 1] === '--discard-prev') {
+                return '';
+            }
+            if (array[index] === '--discard-next') {
+                flagDiscard = true;
+            }
+            if (array[index] === '--double-next') {
+                return array[index + 1]
+            }
+        }
+        if (item === '--double-prev' || item === '--discard-prev' || item === '--discard-next') {
+            return '';
+        }
+        if (item === '--double-next') {
+            return ''
+        }
+        return item
+    }).filter(item => item !== '')
 };
