60422a246a31010dd8a84a4ff98809d863c22331
shinoby85 <ponk@tut.by>
2020-09-07 19:05:42 +0300

    feat: add code to extended-repeat

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
+module.exports = function repeater(str, options) {
+  //throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  let result=''
+  if(options.separator===undefined){
+    options.separator='+'
+  }
+  if(options.additionSeparator===undefined){
+    options.additionSeparator='|'
+  }
+  if (options.repeatTimes===0 || options.repeatTimes===undefined){
+    result+=str
+    if (options.additionRepeatTimes===0 || options.additionRepeatTimes===undefined){
+      result+=options.addition
+    }
+  }
+  for (let i=0;i<options.repeatTimes;i++){
+    result+=str
+    for (let j=0;j<options.additionRepeatTimes;j++){
+      result+=options.addition
+      if (j<options.additionRepeatTimes-1) {
+        result += options.additionSeparator
+      }
+    }
+    if (i<options.repeatTimes-1) {
+      result += options.separator
+    }
+  }
+  return result
 };
   
