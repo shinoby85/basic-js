f5b3abfda315d7f841c1af28234f4bdeb82654a5
shinoby85 <ponk@tut.by>
2020-09-08 21:40:20 +0300

    feat: add checking for a pint to carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -6,8 +6,22 @@ const HALF_LIFE_PERIOD= 5730;
 module.exports = function dateSample(sampleActivity) {
   //throw new CustomError('Not implemented');
   // remove line with error and write your code here
-  if (typeof sampleActivity!=="string" || Number(sampleActivity)>MODERN_ACTIVITY || Number(sampleActivity)<=0 || isNaN(Number(sampleActivity))){
+
+  if (typeof sampleActivity!=="string"){
     return false;
+  }
+  let firstPointFlag=false
+  sampleActivity=sampleActivity.split('').filter(it=>{
+    if (it!=='.' || !firstPointFlag){
+      if (it==='.'){
+        firstPointFlag=true
+      }
+      return true
+    }
+    return false
+  }).join('')
+  if (Number(sampleActivity)>MODERN_ACTIVITY || Number(sampleActivity)<=0 || isNaN(Number(sampleActivity))){
+    return false
   }
   let k=0.693/HALF_LIFE_PERIOD
   let t=Math.log(MODERN_ACTIVITY/Number(sampleActivity))/k
